6d89aa0e807de97c6553e2984c61e18821479f86
Blue <ray1997860102@gmail.com>
2021-04-16 17:43:17 +0800

    google login failed on Iphone

--- routes/googlesignin.js
+++ routes/googlesignin.js
@@ -4,11 +4,9 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-  const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-  const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
   const token = req.body.idtoken;
   try {
-    const client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    const client = new OAuth2Client(CLIENT_ID);
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: CLIENT_ID,
